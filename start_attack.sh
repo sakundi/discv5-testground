8267611b1a5ae3ecf887251c423320a33c435f2d
Andres Gomez <kurono@riseup.net>
2023-01-23 22:01:28 +0000

    improve attack script

--- start_attack.sh
+++ start_attack.sh
@@ -3,3 +3,6 @@
 nohup testground daemon &
 testground run composition -f compositions/eclipse-attack-monopolizing-by-incoming-nodes.toml
 echo "Attack started!..."
+sleep 1000
+docker ps --all | grep Exited | gawk '{print $1}' | xargs sudo docker rm -f
+docker ps --all | grep Created | gawk '{print $1}' | xargs docker rm -
