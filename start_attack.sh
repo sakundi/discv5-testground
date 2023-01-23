34eeca5015d50bc56ec557b1a01900c63c7dcad6
Andres Gomez <kurono@riseup.net>
2023-01-23 22:39:28 +0000

    improve attack script

--- start_attack.sh
+++ start_attack.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Cleaning docker..."
-docker ps --all | grep Exited | gawk '{print $1}' | xargs sudo docker rm -f
-docker ps --all | grep Created | gawk '{print $1}' | xargs docker rm -
+docker ps --all | grep Exited  | gawk '{print $1}' | xargs docker rm -f
+docker ps --all | grep Created | gawk '{print $1}' | xargs docker rm -f
 echo "Starting testground..."
 nohup testground daemon &
 echo "Starting attack!..."
-testground run composition -f compositions/eclipse-attack-monopolizing-by-incoming-nodes.toml
+# testground run composition -f compositions/eclipse-attack-monopolizing-by-incoming-nodes.toml
 echo "Attack started!"
